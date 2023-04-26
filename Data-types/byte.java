6328c39614366d9008a37a8d9ee05e20fe561181
Lealem Birhanu <101050207+lealemb@users.noreply.github.com>
2023-04-26 15:34:53 +0300

    byte data-type

---
+++ Data-types/byte.java
@@ -0,0 +1,9 @@
+//data type can have values from -128 to 127 
+class Main {
+  public static void main(String[] args) {
+
+    byte range;
+    range = 124;
+    System.out.println(range);    // prints 124
+  }
+}
