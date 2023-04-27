8513ce6bc018d8c096a2bdd2043c72a68916cfe1
Lealem Birhanu <101050207+lealemb@users.noreply.github.com>
2023-04-27 23:56:27 +0300

    short-type

---
+++ short.java
@@ -0,0 +1,10 @@
+//short data type in Java can have values from -32768 to 32767
+//(16-bit signed two's complement integer)
+class Main {
+  public static void main(String[] args) {
+    	
+    short temperature;
+    temperature = -200;
+    System.out.println(temperature);  // prints -200
+  }
+}
