d047e5b8516d2d827a7bf70944d630737ad53768
Lealem Birhanu <101050207+lealemb@users.noreply.github.com>
2023-05-03 22:50:05 +0300

    datatypes

---
+++ problems/datatype.java
@@ -0,0 +1,9 @@
+// a Java program to print an int, a double and a char on screen.
+class Ans{
+  public static void main(String[] args){
+    int x = 10;
+    double y = 10.23;
+    char z = 'z';
+    System.out.println("int : "+x+"\nfloat : "+y+"\nchar : "+z);
+  }
+}
