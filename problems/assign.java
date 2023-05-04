e9442ae3fef56d610d0845135d3c5ecdd6c4ef26
Lealem Birhanu <101050207+lealemb@users.noreply.github.com>
2023-05-04 22:11:17 +0300

    solved

---
+++ problems/assign.java
@@ -0,0 +1,9 @@
+// a program  to check if atleast one of the conditions 'a < 50' or 'a < b' is true.
+
+class Ans{
+  public static void main(String[] args){
+    int a = 55;
+    int b = 70;
+    System.out.println(a < 50 && a < b);
+  }
+}
