ec0e5b5b6381d47bdbf03d6a60b3e30ccc2aef0a
Lealem Birhanu <101050207+lealemb@users.noreply.github.com>
2023-05-06 22:20:29 +0300

    solved

---
+++ problems/area.java
@@ -0,0 +1,8 @@
+// a program to print the area of a rectangle of sides 2 and 3 units.
+
+class Ans{
+  public static void main(String[] args){
+    int area = 2*3;
+    System.out.println("The Area of recatngle is : "+area);
+  }
+}
