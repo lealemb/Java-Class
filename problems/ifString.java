e4920109b94504c0d8c93a87d4578657b6e4a553
Lealem Birhanu <101050207+lealemb@users.noreply.github.com>
2023-05-09 23:54:27 +0300

    ifString.java

--- problems/ifString.java
+++ problems/ifString.java
@@ -1,6 +1,6 @@
 class Main {
   public static void main(String[] args) {
-    // create a string variable
+    // create a string variable 
     String language = "Java";
     if (language == "Java") {
       System.out.println("It is a Programming Language");
