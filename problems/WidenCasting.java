a71927e252862c5c90717103bf38ddb6f036fed6
Lealem Birhanu <101050207+lealemb@users.noreply.github.com>
2023-05-11 19:20:30 +0300

    WidenCasting.java

---
+++ problems/WidenCasting.java
@@ -0,0 +1,9 @@
+public class Main {
+  public static void main(String[] args) {
+    int myInt = 11;
+    double myDouble = myInt; // Automatic casting: int to double
+
+    System.out.println(myInt);      // This is original Outputs 9
+    System.out.println(myDouble);   // And after casting into double Outputs 9.0
+  }
+}
