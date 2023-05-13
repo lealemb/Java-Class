7ba13af20f987e3395159877706ba4813454087b
Lealem Birhanu <101050207+lealemb@users.noreply.github.com>
2023-05-13 22:50:48 +0300

    arr.java

---
+++ problems/arr.java
@@ -0,0 +1,13 @@
+class Main {
+ public static void main(String[] args) {
+  
+   // create an array
+   int[] age = {12, 4, 5};
+
+   // access each array elements
+   System.out.println("Accessing Elements of Array:");
+   System.out.println("First Element: " + age[0]);
+   System.out.println("Second Element: " + age[1]);
+   System.out.println("Third Element: " + age[2]);
+ }
+}
