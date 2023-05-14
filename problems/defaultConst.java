adfb4946fe7a78d40da7e249663434018dfc8651
Lealem Birhanu <101050207+lealemb@users.noreply.github.com>
2023-05-14 22:16:25 +0300

    defaultConst.java

---
+++ problems/defaultConst.java
@@ -0,0 +1,11 @@
+class Main {
+  int a;
+  boolean b;
+  public static void main(String[] args) {
+    Main obj = new Main();
+    
+    System.out.println("Default Value:");
+    System.out.println("a = " + obj.a);
+    System.out.println("b = " + obj.b);
+  }
+}
