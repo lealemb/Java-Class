3f1b3323949d5a55de8499390030e4061b58dab0
Lealem Birhanu <101050207+lealemb@users.noreply.github.com>
2023-05-18 12:52:12 +0300

    AddiInput.java

---
+++ problems/AddiInput.java
@@ -0,0 +1,22 @@
+// a java program to add two number input from the keyboared
+
+import java.util.Scanner;
+public class Addition {
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        int number1;
+        int number2;
+        int sum;
+        System.out.print("Ent11er first integer: ");
+        number1 = input.nextInt();
+        System.out.print("Enter second integer: ");
+        number2 = input.nextInt();
+        sum = number1+number2;
+        System.out.printf("sum is
+                          , sum);
+
+
+    }
+
+}
