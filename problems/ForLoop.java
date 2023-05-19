b04b42bbbfd5f23fbb3977fc309b2e5a68886e4b
Lealem Birhanu <101050207+lealemb@users.noreply.github.com>
2023-05-19 22:29:51 +0300

    ForLoop.java

---
+++ problems/ForLoop.java
@@ -0,0 +1,8 @@
+// a Java program to print the numbers from 1 to 10 using a for loop.
+public class PrintNumbers {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
