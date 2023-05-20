1d7265f77c0cdf804765bba11787bcc0bf01509b
Lealem Birhanu <101050207+lealemb@users.noreply.github.com>
2023-05-20 21:49:56 +0300

    CheckTheNum.java

--- problems/CheckTheNum.java
+++ problems/CheckTheNum.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class NumberCheck {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+         
         System.out.print("Enter a number: ");
         int number = scanner.nextInt();
 
