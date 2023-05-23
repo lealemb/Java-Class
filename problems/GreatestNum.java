d0e2686f218ff758858ff9e0e467df1cb8e18548
Lealem Birhanu <101050207+lealemb@users.noreply.github.com>
2023-05-23 20:09:16 +0300

    GreatestNum.java

--- problems/GreatestNum.java
+++ problems/GreatestNum.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
 public class GreatestNum {
     public static void main(String[] args) {
+        
         Scanner in = new Scanner(System.in);
+        
         System.out.print("Input the 1st number: ");
         int num1 = in.nextInt();
         System.out.print("Input the 2nd number: ");
@@ -11,13 +13,17 @@ public class GreatestNum {
         
         if (num1 > num2)
             if (num1 > num3)
+                
                 System.out.println("The greatest: " + num1);
 
         if (num2 > num1)
             if (num2 > num3)
+                
                 System.out.println("The greatest: " + num2);
+        
         if (num3 > num1)
             if (num3 > num2)
+                
                 System.out.println("The greatest: " + num3);
     }
 }
