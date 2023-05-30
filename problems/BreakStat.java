8c192a1a1157e5ed263cb9e03dade49c071802f9
Lealem Birhanu <101050207+lealemb@users.noreply.github.com>
2023-05-30 23:55:15 +0300

    BreakStat.java

--- problems/BreakStat.java
+++ problems/BreakStat.java
@@ -3,7 +3,7 @@ class BreakStat {
         for (int i = 1; i <= 10; ++i) { 
             if (i == 5) {
                 break;
-            }      
+            }       
             System.out.println(i);
         }   
     }
