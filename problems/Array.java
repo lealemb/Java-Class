a71810d9146452d50465cccac9de1352fa74b58c
Lealem Birhanu <101050207+lealemb@users.noreply.github.com>
2023-06-04 23:37:05 +0300

    Create Array.java

---
+++ problems/Array.java
@@ -0,0 +1,10 @@
+public class Array {
+
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5};
+
+        for (int val: array) {
+            System.out.println(val);
+        }
+    }
+}
