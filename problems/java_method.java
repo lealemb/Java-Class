2d8eb56f14d40526bd5ef8d01c826de562db72e8
Lealem Birhanu <101050207+lealemb@users.noreply.github.com>
2023-06-12 23:26:03 +0300

    java_method.java

---
+++ problems/java_method.java
@@ -0,0 +1,10 @@
+public class java_method {
+    static int add(int a, int b){
+       return a+b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(add(34,44));
+    }
+
+}
